8a7644e78dd0818978f7fda8fdc19b5c7ac80d5c
Steve <steve.robinson290391@gmail.com>
2017-03-30 11:36:03 +0100

    Only expose component getter for Store

--- core/src/main/scala/com/onsightgames/scalalibgdx/Store.scala
+++ core/src/main/scala/com/onsightgames/scalalibgdx/Store.scala
@@ -2,14 +2,16 @@ package com.onsightgames.scalalibgdx
 
 import com.onsightgames.scalalibgdx.events.Event
 
-class Store(var components : Set[Component[_ <: Entity]]) {
+case class Store(private var _components : Set[Component[_ <: Entity]]) {
+
+  def components : Set[Component[_ <: Entity]] = _components
 
   def dispatch(event : Event) : Unit = {
-    components = components.map(_ update event)
+    _components = _components.map(_ update event)
   }
 
   def getState : Iterable[Entity] = {
-    components.map(_.state)
+    _components.map(_.state)
   }
 
 }
