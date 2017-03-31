922a19884be9fd2691f5790572bbf5cd138b8447
Daniel Patterson <dan24patt@gmail.com>
2017-03-31 16:37:55 +0100

    Use boundary collision detection to reverse alien direction
    So that the same logic is used across entities to determin boundary
    collisions

--- core/src/main/scala/com/onsightgames/scalalibgdx/aliens/AlienFleetReducer.scala
+++ core/src/main/scala/com/onsightgames/scalalibgdx/aliens/AlienFleetReducer.scala
@@ -1,12 +1,24 @@
 package com.onsightgames.scalalibgdx.aliens
 
 import com.onsightgames.scalalibgdx.Reducer
+import com.onsightgames.scalalibgdx.events.BoundaryCollisionDetector.{Boundary, BoundaryTouched}
 import com.onsightgames.scalalibgdx.events.Event
 import com.onsightgames.scalalibgdx.events.LifecycleEventEmitter.Update
 
 object AlienFleetReducer extends Reducer[AlienFleet] {
 
   override def reduce: PartialFunction[(AlienFleet, Event), AlienFleet] = {
-    case (alienFleet, Update(timeElapsed)) => alienFleet.update(timeElapsed)
+    case (fleet, Update(timeElapsed)) => fleet.update(timeElapsed)
+
+    case (fleet, boundaryTouched : BoundaryTouched) if shouldChangeXSpeed(boundaryTouched, fleet) =>
+      val velocity = fleet.velocity
+      fleet.copy(velocity = velocity.copy(x = -velocity.x))
+  }
+
+  private def shouldChangeXSpeed(boundaryTouched: BoundaryTouched, fleet: AlienFleet) : Boolean = {
+    boundaryTouched.isTarget(fleet) && (boundaryTouched.boundary match {
+      case Boundary.Left | Boundary.Right => true
+      case _                              => false
+    })
   }
 }
